3d261ee6bb7fdc4b5ea40c4c31404283f8dbd8c0
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2019-11-24 04:54:45 +0530

    Started with FormView for handling Forms

--- event_manager/management/views.py
+++ event_manager/management/views.py
@@ -1,11 +1,13 @@
 from django.views.generic import TemplateView
 from management.forms import VisitVisitorModelForm
+from django.views.generic.edit import FormView
 
 
-class NewVisitAndVisitorView(TemplateView):
+class NewVisitAndVisitorView(FormView):
     template_name = 'new_visit.html'
+    form_class = VisitVisitorModelForm
+    success_url = '/'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = VisitVisitorModelForm()
-        return context
+    def form_valid(self, form):
+        import pdb; pdb.set_trace()
+        return super(NewVisitAndVisitorView, self).form_valid(form)
