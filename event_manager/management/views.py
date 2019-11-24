4ef983d046d9fcc330ee9475332736ace07d75be
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2019-11-25 03:48:44 +0530

    Started (yet Incomplete) Form Validation with MultiModel Forms

--- event_manager/management/views.py
+++ event_manager/management/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
-from management.forms import VisitVisitorModelForm
+from management.forms import VisitVisitorModelForm, VisitorModelForm, VisitModelForm
 from django.views.generic.edit import FormView
+from django.forms.utils import ErrorDict
 
 
 class NewVisitAndVisitorView(FormView):
@@ -8,6 +10,50 @@ class NewVisitAndVisitorView(FormView):
     form_class = VisitVisitorModelForm
     success_url = '/'
 
-    def form_valid(self, form):
-        import pdb; pdb.set_trace()
-        return super(NewVisitAndVisitorView, self).form_valid(form)
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        visit_and_visitor_raw_data = dict(form.data)
+
+        map_field_name = {
+            'email': 'visitor-email',
+            'first_name': 'visitor-first_name',
+            'last_name': 'visitor-last_name',
+            'phone_number': 'visitor-phone_number',
+            'in_time': 'visit-in_time',
+            'purpose': 'visit-purpose',
+            'host': 'visit-host'
+        }
+
+        visitor_email = visit_and_visitor_raw_data['visitor-email'][0]
+        visitor_first_name = visit_and_visitor_raw_data['visitor-first_name'][0]
+        visitor_last_name = visit_and_visitor_raw_data['visitor-last_name'][0]
+        visitor_phone_number = visit_and_visitor_raw_data['visitor-phone_number'][0]
+        visit_in_time = visit_and_visitor_raw_data['visit-in_time'][0]
+        visit_purpose = visit_and_visitor_raw_data['visit-purpose'][0]
+        visit_host = visit_and_visitor_raw_data['visit-host'][0]
+
+        visitor_data = {
+            'email': visitor_email,
+            'first_name': visitor_first_name,
+            'last_name': visitor_last_name,
+            'phone_number': visitor_phone_number,
+        }
+
+        form_errors = {}
+        visitor_form = VisitorModelForm(data=visitor_data)
+        if not visitor_form.is_valid():
+            for field, field_error in visitor_form.errors.items():
+                form_errors[map_field_name[field]] = field_error
+        else:
+            visitor = visitor_form.save()
+            visit_details = {
+                'in_time': visit_in_time,
+                'purpose': visit_purpose,
+                'host': visit_host,
+            }
+            return HttpResponseRedirect(self.get_success_url())
+
+        form_errors = ErrorDict(form_errors)
+        form._errors = form_errors
+
+        return self.form_invalid(form)
