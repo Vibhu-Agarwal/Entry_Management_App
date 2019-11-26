b85fdbe03b96c0a6efc34d84a05d7c792b5159f7
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2019-11-26 17:10:35 +0530

    Completed [w/out APIs calling] Form-Validation structure for NewVisit

--- event_manager/management/views.py
+++ event_manager/management/views.py
@@ -1,12 +1,19 @@
+from users.models import User
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
 from api.permissions import IsHostOrLoggedOutMixin
 from django.views.generic import TemplateView
 from management.forms import VisitVisitorModelForm, VisitorModelForm, VisitModelForm
 from django.views.generic.edit import FormView
 
+HOST_REPR = settings.HOST_REPR
+
 
 class NewVisitAndVisitorView(IsHostOrLoggedOutMixin, FormView):
     template_name = 'new_visit.html'
     success_url = '/'
+    employee_sign_in_page = '/sign-in/'
 
     def get_user(self):
         user = self.request.user
@@ -39,3 +46,34 @@ class NewVisitAndVisitorView(IsHostOrLoggedOutMixin, FormView):
             print(visit_details)
 
         return super(NewVisitAndVisitorView, self).form_valid(form)
+
+    def form_invalid(self, form):
+
+        visitor_form = form.forms['visitor']
+        visitor_form_errors = visitor_form.errors
+        visitor_email_errors = visitor_form_errors.get('email', None)
+
+        if visitor_email_errors:
+            old_user_error = 'User with this Email address already exists.'
+
+            if old_user_error in visitor_email_errors:
+
+                # Removing old_user_error from visitor form errors
+                visitor_form._errors['email'] = visitor_form.error_class()
+                for visitor_email_error in visitor_email_errors:
+                    if old_user_error != visitor_email_error:
+                        visitor_form.add_error('email', visitor_email_error)
+                length_visitor_email_errors = len(visitor_email_errors)
+
+                if length_visitor_email_errors == 0:
+                    visitor_form._errors.pop('email')
+                    visitor_email = form.data['visitor-email']
+                    visitor = User.objects.get(email=visitor_email)
+                    if visitor.user_type == 'visitor':
+                        # Old Visitor
+                        pass
+                    elif visitor.user_type == HOST_REPR:
+                        return HttpResponseRedirect(self.employee_sign_in_page)
+                    else:
+                        raise PermissionDenied()
+        return super(NewVisitAndVisitorView, self).form_invalid(form)
